fb12e800f04c458f07e251d019d1650cd34d6d39
gabs12306 <sakura123gabi@gmail.com>
2025-09-30 15:11:15 -0500

    Se agrega soporte para CORS en la configuración del servidor

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { conectarBD } from './config/conexion.js';
 import router from './routes/authRoutes.js';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 app.use(express.json());
 conectarBD();
 app.use('/Views', router);
